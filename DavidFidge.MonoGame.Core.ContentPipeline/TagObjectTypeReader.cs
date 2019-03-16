7fc836c35a309a104f0c9282e426748da9385e0a
David Fidge <dfidge@internode.on.net>
2019-03-16 10:53:14 +1030

    Add custom content pipeline pricessor for rectangular bounding box and custom effect processing

---
+++ DavidFidge.MonoGame.Core.ContentPipeline/TagObjectTypeReader.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace DavidFidge.MonoGame.Core.ContentPipeline
+{
+    public class TagObjectTypeReader : ContentTypeReader<TagObject>
+    {
+        protected override TagObject Read(ContentReader contentReader, TagObject tagObject)
+        {
+            var boundingBox = contentReader.ReadObject<BoundingBox>();
+
+            if (tagObject == null)
+                tagObject = new TagObject(boundingBox);
+            else
+                tagObject.BoundingBox = boundingBox;
+
+            return tagObject;
+        }
+    }
+}
