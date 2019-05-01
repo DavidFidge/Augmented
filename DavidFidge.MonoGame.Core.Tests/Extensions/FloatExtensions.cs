5c8858b6381cf78ffcbbb02ba615b1c80c67105b
David Fidge <dfidge@internode.on.net>
2019-05-02 08:12:46 +0930

    Add VertexExtensions tests for generating normals from triangle strip when strip contains lines

--- DavidFidge.MonoGame.Core.Tests/Extensions/FloatExtensions.cs
+++ DavidFidge.MonoGame.Core.Tests/Extensions/FloatExtensions.cs
@@ -8,6 +8,24 @@ namespace DavidFidge.MonoGame.Core.Tests.Services
     {
         public static void IsEquivalentTo(this float source, float equivalent)
         {
+            if (float.IsNaN(source) || float.IsNaN(equivalent))
+            {
+                Assert.IsTrue(float.IsNaN(source) && float.IsNaN(equivalent));
+                return;
+            }
+
+            if (float.IsNegativeInfinity(source) || float.IsNegativeInfinity(equivalent))
+            {
+                Assert.IsTrue(float.IsNegativeInfinity(source) && float.IsNegativeInfinity(equivalent));
+                return;
+            }
+
+            if (float.IsPositiveInfinity(source) || float.IsPositiveInfinity(equivalent))
+            {
+                Assert.IsTrue(float.IsPositiveInfinity(source) && float.IsPositiveInfinity(equivalent));
+                return;
+            }
+
             Assert.IsTrue(Math.Abs(source - equivalent) < 0.000001f);
         }
     }
--- DavidFidge.MonoGame.Core.Tests/Extensions/VertexExtensionsTests.cs
+++ DavidFidge.MonoGame.Core.Tests/Extensions/VertexExtensionsTests.cs
@@ -91,5 +91,102 @@ namespace DavidFidge.MonoGame.Core.Tests.Services
 
             vertexPositionNormalTextures[3].Normal.IsEquivalentTo(Vector3.Normalize(expectedNormalTriangle2));
         }
+
+        [TestMethod]
+        public void GenerateNormalsForTriangleStrip_Should_Do_Nothing_If_Straight_Line()
+        {
+            // Arrange
+            var vertexPositionNormalTextures = new VertexPositionNormalTexture[4];
+
+            vertexPositionNormalTextures[0] = new VertexPositionNormalTexture(
+                new Vector3(0, 0, 0),
+                Vector3.Zero,
+                Vector2.Zero
+                );
+
+            vertexPositionNormalTextures[1] = new VertexPositionNormalTexture(
+                new Vector3(1, 1, 1),
+                Vector3.Zero,
+                Vector2.Zero
+            );
+
+            vertexPositionNormalTextures[2] = new VertexPositionNormalTexture(
+                new Vector3(0, 0, 0),
+                Vector3.Zero,
+                Vector2.Zero
+            );
+            
+            var indices = new[] { 0, 1, 2 };
+
+            // Act
+            vertexPositionNormalTextures = vertexPositionNormalTextures.GenerateNormalsForTriangleStrip(indices);
+
+            // Assert
+            var nanNormal = Vector3.Normalize(Vector3.Zero);
+
+            nanNormal.IsEquivalentTo(vertexPositionNormalTextures[0].Normal);
+            nanNormal.IsEquivalentTo(vertexPositionNormalTextures[1].Normal);
+            nanNormal.IsEquivalentTo(vertexPositionNormalTextures[2].Normal);
+        }
+
+        [TestMethod]
+        public void GenerateNormalsForTriangleStrip_Should_Not_Combine_Normal_With_Value_And_NAN_Normal()
+        {
+            //        3
+            //       /|
+            //      / |
+            //     /  |
+            //    /  _|
+            // 1 /__/ 2,4
+            //
+            // Arrange
+            var vertexPositionNormalTextures = new VertexPositionNormalTexture[4];
+
+            vertexPositionNormalTextures[0] = new VertexPositionNormalTexture(
+                new Vector3(0, 0, 0),
+                Vector3.Zero,
+                Vector2.Zero
+                );
+
+            vertexPositionNormalTextures[1] = new VertexPositionNormalTexture(
+                new Vector3(1, 1, 0),
+                Vector3.Zero,
+                Vector2.Zero
+            );
+
+            vertexPositionNormalTextures[2] = new VertexPositionNormalTexture(
+                new Vector3(1, 1, 1),
+                Vector3.Zero,
+                Vector2.Zero
+            );
+
+            vertexPositionNormalTextures[3] = new VertexPositionNormalTexture(
+                new Vector3(1, 1, 0),
+                Vector3.Zero,
+                Vector2.Zero
+            );
+
+            var indices = new[] { 0, 1, 2, 3 };
+
+            // Act
+            vertexPositionNormalTextures = vertexPositionNormalTextures.GenerateNormalsForTriangleStrip(indices);
+
+            // Assert
+            // 3 to 2 and 3 to 1
+            var expectedNormalTriangle1 = Vector3.Cross(
+                new Vector3(0, 0, 1),
+                new Vector3(1, 1, 1)
+            );
+
+            var nanNormal = Vector3.Normalize(Vector3.Zero);
+
+            vertexPositionNormalTextures[0].Normal.IsEquivalentTo(Vector3.Normalize(expectedNormalTriangle1));
+
+            vertexPositionNormalTextures[1].Normal.IsEquivalentTo(Vector3.Normalize(expectedNormalTriangle1));
+
+            vertexPositionNormalTextures[2].Normal.IsEquivalentTo(Vector3.Normalize(expectedNormalTriangle1));
+
+            vertexPositionNormalTextures[3].Normal.IsEquivalentTo(nanNormal);
+        }
     }
 }
