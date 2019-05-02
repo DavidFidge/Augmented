c652a5daa0e2fe48b5858c6d7a99967126b02c3e
David Fidge <dfidge@internode.on.net>
2019-05-02 18:55:04 +0930

    SurroundingPoints additional tests

--- DavidFidge.MonoGame.Core.Tests/Extensions/PointExtensionsTests.cs
+++ DavidFidge.MonoGame.Core.Tests/Extensions/PointExtensionsTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 using DavidFidge.MonoGame.Core.Graphics.Extensions;
 using DavidFidge.TestInfrastructure;
@@ -38,10 +39,112 @@ namespace DavidFidge.MonoGame.Core.Tests.Services
                 new Point(0, 1),
                 new Point(1, -1),
                 new Point(1, 0),
+                new Point(1, 1)
+            };
+
+            CollectionAssert.AreEquivalent(expectedPoints, result);
+        }
+
+        [TestMethod]
+        public void SurroundingPoints_Should_Return_All_Surrounding_Points_Limited_By_X_Min()
+        {
+            // Arrange
+            var point = new Point();
+
+            // Act
+            var result = point.SurroundingPoints(0);
+
+            // Assert
+            var expectedPoints = new List<Point>
+            {
+                new Point(0, -1),
+                new Point(0, 1),
+                new Point(1, -1),
+                new Point(1, 0),
                 new Point(1, 1),
             };
 
             CollectionAssert.AreEquivalent(expectedPoints, result);
+        }
+
+
+        [TestMethod]
+        public void SurroundingPoints_Should_Return_All_Surrounding_Points_Limited_By_X_Max()
+        {
+            // Arrange
+            var point = new Point();
+
+            // Act
+            var result = point.SurroundingPoints(xMax: 0);
+
+            // Assert
+            var expectedPoints = new List<Point>
+            {
+                new Point(-1, -1),
+                new Point(-1, 0),
+                new Point(-1, 1),
+                new Point(0, -1),
+                new Point(0, 1)
+            };
+
+            CollectionAssert.AreEquivalent(expectedPoints, result);
+        }
+
+        [TestMethod]
+        public void SurroundingPoints_Should_Return_All_Surrounding_Points_Limited_By_Y_Min()
+        {
+            // Arrange
+            var point = new Point();
+
+            // Act
+            var result = point.SurroundingPoints(yMin: 0);
+
+            // Assert
+            var expectedPoints = new List<Point>
+            {
+                new Point(-1, 0),
+                new Point(-1, 1),
+                new Point(0, 1),
+                new Point(1, 0),
+                new Point(1, 1)
+            };
+
+            CollectionAssert.AreEquivalent(expectedPoints, result);
+        }
+
+        [TestMethod]
+        public void SurroundingPoints_Should_Return_All_Surrounding_Points_Limited_By_Y_Max()
+        {
+            // Arrange
+            var point = new Point();
+
+            // Act
+            var result = point.SurroundingPoints(yMax: 0);
+
+            // Assert
+            var expectedPoints = new List<Point>
+            {
+                new Point(-1, -1),
+                new Point(-1, 0),
+                new Point(0, -1),
+                new Point(1, -1),
+                new Point(1, 0)
+            };
+
+            CollectionAssert.AreEquivalent(expectedPoints, result);
+        }
+
+        [TestMethod]
+        public void SurroundingPoints_Should_Return_No_Points_If_Limit_Excludes_All()
+        {
+            // Arrange
+            var point = new Point();
+
+            // Act
+            var result = point.SurroundingPoints(2);
+
+            // Assert
+            Assert.IsTrue(result.IsEmpty());
         }
     }
 }
